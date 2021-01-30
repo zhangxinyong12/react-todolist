d7c65e0245ce14f29421b85804014e3173446482
xuwen_cn <603187090@qq.com>
2021-01-30 14:00:41 +0800

    Update update-name.js

--- src/views/update-name/update-name.js
+++ src/views/update-name/update-name.js
@@ -2,13 +2,16 @@ import { useState } from 'react';
 import styleModule from './update-name.module.scss';
 
 export default function (props) {
-    const { item, submit } = props;
+    const { item, submit, currentIndex } = props;
     const [nameItem, setNameItem] = useState(item.name);
 
     const handleSubmit = (event) => {
+        console.log('111',currentIndex,nameItem)
         event.preventDefault();
-        submit({ ...item, name: nameItem });
-        console.log({ ...item, name: nameItem });
+        // submit({ ...item, name: nameItem });
+        // console.log({ ...item, name: nameItem });
+        // 更新数据
+        submit({...item, name: nameItem},currentIndex)
     };
 
     const handleChange = (event) => {

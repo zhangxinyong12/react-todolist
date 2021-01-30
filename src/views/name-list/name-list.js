fdf552c80147779cbcc104bced0f522bde6fdc94
xuwen_cn <603187090@qq.com>
2021-01-30 13:59:47 +0800

    Update name-list.js

--- src/views/name-list/name-list.js
+++ src/views/name-list/name-list.js
@@ -1,18 +1,31 @@
 import styleModule from './name-list.module.scss';
 import Button from '../../components/button';
 import UpdateName from '../update-name/update-name';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 
 export default function (props) {
     const [show, setShow] = useState(false);
     const [item, setTtem] = useState({});
-    const { list } = props;
-    const showUpdate = (item) => {
+    const [currentIndex, setCurrentIndex] = useState('')
+    const [listArr, setListArr] = useState([])
+
+    useEffect(()=>{
+        setListArr(props.list)
+    },[props.list])
+    // const { list } = props;
+    const showUpdate = (item,index) => {
         setShow(true);
         setTtem(item);
+        setCurrentIndex(index)
     };
-    const listEle = list.map((item, index) => (
-        <li className={styleModule.list} key={item.name + item.age}>
+    const upDateList = (data,index) => {
+        // console.log(data,index)
+        listArr.splice(index,0,data)
+        listArr.splice(index+1, 1)
+        setListArr([...listArr])
+    }
+    const listEle = listArr.map((item, index) => (
+        <li  key={item.name + item.age}>
             <span>名字：{item.name}</span>&nbsp;&nbsp;&nbsp;&nbsp;
             <span >年龄：{item.age}</span>
             <Button onClick={showUpdate.bind(this, item, index)}>修改</Button>
@@ -23,7 +36,7 @@ export default function (props) {
             <ul>
                 {listEle}
             </ul>
-            {show ? <UpdateName item={item} submit={setTtem}></UpdateName> : ''}
+            {show ? <UpdateName item={item} submit={upDateList} currentIndex={currentIndex}></UpdateName> : ''}
         </div>
     );
 }
